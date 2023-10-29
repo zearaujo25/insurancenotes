75eca1a6d3115903fc4a240562e3df166bcc7417
zearaujo25 <joselucasdeagc@gmail.com>
2023-10-29 17:47:43 -0300

    Wiring up database and view

--- src/main/java/com/course/insurancenotes/notes/data/NoteEntity.java
+++ src/main/java/com/course/insurancenotes/notes/data/NoteEntity.java
@@ -4,16 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
-@Entity
+@Entity(name = "NOTE")
 public class NoteEntity {
     @Id
     @GeneratedValue
     private Long id;
-    private String name;
+    private String note;
 
-    public NoteEntity(Long id, String name) {
+    private Long userId;
+
+    public NoteEntity(Long id, String note, Long userId) {
         this.id = id;
-        this.name = name;
+        this.note = note;
+        this.userId = userId;
     }
 
     public NoteEntity() {
@@ -27,11 +30,19 @@ public class NoteEntity {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getNote() {
+        return note;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 }
--- src/main/resources/db/migration/V1__Create_note_table.sql
+++ src/main/resources/db/migration/V1__Create_note_table.sql
@@ -1,5 +1,5 @@
 create table NOTE (
     ID int not null,
-    PERSON_ID int not null,
+    USER_ID int not null,
     NOTE varchar(255) NOT NULL
 );
